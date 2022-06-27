5cd50de28b0bf6e51698f38e21e48e1caf83c20e
Julian Murphy <njmdistrisoft@gmail.com>
2022-06-27 13:35:15 -0300

    delete spaces

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import ChallengeService from './services/ChallengeService';
 import BlocksRepositoryAPI from './repositories/BlocksRepositoryAPI';
 
-
-
-
 let indexValueToExpected = 0;
 let service = new ChallengeService(new BlocksRepositoryAPI());
 let sortedBlocks = service.execute();
